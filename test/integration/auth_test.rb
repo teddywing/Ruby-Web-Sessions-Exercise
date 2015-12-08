6eef2d15e7935dc21580dd7c9e4042d3b8564b4d
Teddy Wing <git@teddywing.com>
2015-12-08 01:17:26 -0500

    auth_test.rb: Create login helpers
    Create a couple new methods to extract login functionality and remove
    duplicate test code.

--- test/integration/auth_test.rb
+++ test/integration/auth_test.rb
@@ -2,27 +2,32 @@ require 'integration_test_helper'
 
 
 class TestAuth < CapybaraTestCase
-  def setup
+  def login(username, password)
     visit '/'
+    fill_in 'username', :with => username
+    fill_in 'password', :with => password
+    click_button 'Login'
   end
 
+  def login_with_correct_credentials
+    login('hubertfarnsworth', 'secret')
+  end
+
+
   def test_index_has_login_form
+    visit '/'
     assert_text 'Login'
     assert has_selector?('//form')
   end
 
   def test_unrecognised_user_login
-    fill_in 'username', :with => 'unregistered'
-    fill_in 'password', :with => 'password'
-    click_button 'Login'
+    login('unregistered', 'password')
 
     assert_text 'Unrecognized user'
   end
 
   def test_login
-    fill_in 'username', :with => 'hubertfarnsworth'
-    fill_in 'password', :with => 'secret'
-    click_button 'Login'
+    login_with_correct_credentials
 
     assert_equal 200, page.status_code
   end
